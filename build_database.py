3c3c294d8784d06ce34def3e98cf16f408877241
Lei <michael.82.sh@gmail.com>
2020-08-07 22:30:01 +0800

    csv to build database

--- build_database.py
+++ build_database.py
@@ -1,37 +1,26 @@
 import os
 from config import db
 from models import Person, Flight
-
-# Data to initialize database with
-PEOPLE = [
-    {"fname": "Doug", "lname": "Farrell"},
-    {"fname": "Kent", "lname": "Brockman"},
-    {"fname": "Bunny", "lname": "Easter"},
-]
-
-FLIGHTS = [
-    {"origin": "New York", "destination": "London",  "duration": "415"},
-    {"origin": "Shanghai", "destination": "Paris",  "duration": "760"},
-    {"origin": "Istanbul", "destination": "Tokyo",  "duration": "700"},
-    {"origin": "New York", "destination": "Paris",  "duration": "435"},
-    {"origin": "Moscow", "destination": "Paris",  "duration": "245"},
-    {"origin": "Lima", "destination": "New York",  "duration": "455"}
-]
+import csv
 
 # Delete database file if it exists currently
-if os.path.exists("people.db"):
-    os.remove("people.db")
+if os.path.exists("data.db"):
+    os.remove("data.db")
 
 # Create the database
 db.create_all()
 
-# iterate over the PEOPLE structure and populate the database
-for person in PEOPLE:
-    p = Person(lname=person.get("lname"), fname=person.get("fname"))
-    db.session.add(p)
+# https://realpython.com/python-csv/
+with open('people.csv') as csv_file:
+    reader = csv.reader(csv_file, delimiter=',')
+    for first_name, last_name in reader:
+        people = Person(fname=first_name, lname=last_name)
+        db.session.add(people)
 
-for flight in FLIGHTS:
-    f = Flight(origin=flight.get("origin"), destination=flight.get("destination"), duration=flight.get("duration"))
-    db.session.add(f)
+with open('flights.csv') as csv_file:
+    reader = csv.reader(csv_file, delimiter=',')
+    for origin, destination, duration in reader:
+        flight = Flight(origin=origin, destination=destination, duration=duration)
+        db.session.add(flight)
 
 db.session.commit()
