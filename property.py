ab7107fc7eb8d925fd97b6a59a3443ada29cebe0
Lei <michael.82.sh@gmail.com>
2020-08-08 12:53:02 +0800

    adapter to PropertyCross app

--- property.py
+++ property.py
@@ -3,7 +3,7 @@ This is the people module and supports all the REST actions for the
 people data
 """
 
-from flask import make_response, abort
+from flask import make_response, abort, jsonify
 from config import db
 from models import Property, PropertySchema
 
@@ -16,16 +16,23 @@ def read_all():
     :return:        json string of list of property
     """
     # Create the list of people from our data
-    properties = Property.query.with_entities(Property.property_id,
-                                              Property.title, 
-                                              Property.img_url, 
-                                              Property.price_formatted, 
-                                              Property.bedroom_number, 
-                                              Property.bathroom_number, 
-                                              Property.summary).order_by(Property.property_id).all()
+    # .with_entities(Property.property_id,
+    #                                           Property.title, 
+    #                                           Property.img_url, 
+    #                                           Property.price_formatted, 
+    #                                           Property.bedroom_number, 
+    #                                           Property.bathroom_number, 
+    #                                           Property.summary)
+    properties = Property.query.order_by(Property.property_id).all()
 
     # Serialize the data for the response
     property_schema = PropertySchema(many=True)
     data = property_schema.dump(properties)
-    return data
+    #return data
+    return jsonify({"response": {
+                        "application_response_code": "100",
+                        "page": 1,
+                        "total_results": 100,
+                        "listings" : data}}
+                        )
 
